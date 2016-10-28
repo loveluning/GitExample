f71383f4b9e6975fec2c3f04255ae384513230a6
淘发现1 <taofaxian1@taofaxian1deiMac.local>
2016-10-28 17:30:08 +0800

    no message

--- GitExample/GitExample/ViewController.m
+++ GitExample/GitExample/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.nn
     
-    NSLog(<#NSString * _Nonnull format, ...#>)
+    NSLog(@"dafafafa");
 }
 
 - (void)didReceiveMemoryWarning {
